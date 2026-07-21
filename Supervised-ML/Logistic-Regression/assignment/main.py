e189f513e14c08f17acf4e29d3fbcfbce8b686d1
Deepthanush1337 <deepthanush3301@icloud.com>
2026-07-21 18:38:21 +0530

    update assignment

--- Supervised-ML/Logistic-Regression/assignment/main.py
+++ Supervised-ML/Logistic-Regression/assignment/main.py
@@ -15,7 +15,7 @@ x_train, x_test, y_train, y_test = train_test_split(x,y, test_size=0.2)
 l1_model = LogisticRegression(penalty="l1", max_iter=1000, solver="liblinear")
 l1_model.fit(x_train, y_train)
 y_pred = l1_model.predict(x_test)
-print(f"Accuracy: {accuracy_score(y_test, y_pred)}, Precision: {precision_score(y_test, y_pred)}")
+print(f"Accuracy : {accuracy_score(y_test, y_pred)}, Precision : {precision_score(y_test, y_pred)}")
 
 l2_model = LogisticRegression(penalty="l2", max_iter=1000)
 l2_model.fit(x_train, y_train)
